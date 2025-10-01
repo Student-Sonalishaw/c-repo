12a47db16fb3ea04a23b31dbaad8a297d10b85fc
Student-Sonalishaw <sonalishaw311@gmail.com>
2025-10-01 16:34:26 +0530

    changed some code

--- OOPs/basics2.cpp
+++ OOPs/basics2.cpp
@@ -5,24 +5,32 @@ using namespace std;
 class Student{
     public:
     string name;
-    double cgpa;
+    /* double cgpa; */
+    double* cgpaPtr;
     //Constructor
     Student( string name, double cgpa){
         this->name = name; //this is a pointer in c++ that points to the current object
-        this->cgpa = cgpa;
+        /* this->cgpa = cgpa; */
+        cgpaPtr = new double;
+        *cgpaPtr = cgpa;
     }
-
+    //copy constructor made by own
+    Student(Student &obj) {
+        this->name = obj.name;
+        this->cgpaPtr = obj.cgpaPtr;
+    }
     void getInfo(){
         cout<<"Name: "<<name<<endl;
-        cout<<"cgpa: "<<cgpa<<endl;
+        cout<<"cgpa: "<<*cgpaPtr<<endl;
     }
 };
 
 int main(){
     Student s1("Sonali shaw",9.9);
-    s1.getInfo();
-    /* Student s2(s1);
-    s2.getInfo(); */ //copy consructor
+    /* s1.getInfo(); */
+     //copy consructor
+    Student s2(s1);
+    s2.getInfo(); 
 
     return 0;
 }
