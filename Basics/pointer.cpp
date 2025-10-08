5dbd388c761c9aa0e24cbf98ed423c619bb05b21
Student-Sonalishaw <sonalishaw311@gmail.com>
2025-10-08 20:22:42 +0530

    new commit

--- Basics/pointer.cpp
+++ Basics/pointer.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     int x = 3;
-    int* ptr = &x; //&x is the adress of x, &ptr is also printed the adress of x
+    int *ptr = &x; //&x is the adress of x, &ptr is also printed the adress of x
     cout<<&x<<endl; //int* is the pointer for store the adress of x
     cout<<ptr<<endl;// if we print *ptr, so *ptr is the value of x without using x.
     // if we want to change the value of x without using x then,
