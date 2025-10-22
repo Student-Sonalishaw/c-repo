4a16acbec544b5212836a2549943cb362be8f2f9
Student-Sonalishaw <sonalishaw311@gmail.com>
2025-10-22 16:25:15 +0530

    some changes commit

--- OOPs/staticKeyword1.cpp
+++ OOPs/staticKeyword1.cpp
@@ -1,16 +1,25 @@
 #include<iostream>
 #include<string.h>
 using namespace std;
-//Stactic variable for functions
+//normal functions without static keyword
 void fun(){
-    static int x = 0; // for print this statement one time
-    cout<<"x : "<<x<<endl;
+    int x = 0;
+    cout<<"x: "<<x<<endl;
     x++;
 }
+//Stactic variable for functions
+void fun1(){
+    static int y = 0; // for print this statement one time
+    cout<<"y : "<<y<<endl;
+    y++;
+}
 int main(){
+    fun();//don't increases value
     fun();
     fun();
-    fun();//increased value because of static
+    fun1();
+    fun1();
+    fun1();//increased value because of static
 
     return 0;
 }
